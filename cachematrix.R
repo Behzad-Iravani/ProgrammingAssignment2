413229e4799f50da854fcf94e435491b49fa4782
Behzad Iravani <behzadiravani@gmail.com>
2014-07-26 21:17:24 +0430

    Update CacheMatrix.M, adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,18 +3,18 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {    ## MakeCacheMatrix is a function to cache its inverse 
 
-  inv <- NULL
+  inv <- NULL     ## firstly, an undentified value (NULL) is assigned to inverse as the default value 
   
-  set <- function (y){
+  set <- function (y){     ## a function named set is defined to set the matrix
           x <<- y
           inv <<- NULL
     } 
-  get <- function() x
-  setinv <- function(inverse) inv<<- inverse
-  getinv <- function() inv
-  list(set = set, get = get,
+  get <- function() x   ## This function is defined to read the matrix
+  setinv <- function(inverse) inv<<- inverse   ## This function assigns the inverse of the matrix to inv
+  getinv <- function() inv     ## This function read the value of inv 
+  list(set = set, get = get, ## The output of this function is a list that contains 4 functions defined previously 
        setinv = setinv,
        getinv = getinv)
 }
@@ -24,13 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  inv <- x$getinv()
-  if(!is.null(inv)) {
-    message("getting cached data")
+  inv <- x$getinv()     ## read the inverse of the matrix from the list that is the output of the makeCacheMatrix function 
+  if(!is.null(inv)) {   ## check if the value of inv is Null or not 
+    message("getting cached data") ## if it is not Null then it return the cache value 
     return(inv)
   }
-  data <- x$get()
-  inv <- solve(data, ...)
+  data <- x$get() ## else if the value of inv is not defined this value computed using solve function
+  inv <- solve(data, ...) 
   x$setinv(inv)
   inv
 }
